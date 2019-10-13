b140661cbcd843e1d912e9b79ebc24bd2bc5bd67
kevin <kev.ubedo@gmail.com>
2019-10-13 16:39:23 -0600

    Avances home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,18 @@
 <template>
-  <HelloWorld />
+<div>
+  <Header></Header>
+  <HelloWorld></HelloWorld>
+</div>
 </template>
 
 <script>
 import HelloWorld from '../components/HelloWorld';
+import Header from '../components/Header';
 
 export default {
   components: {
     HelloWorld,
+    Header
   },
 };
 </script>
