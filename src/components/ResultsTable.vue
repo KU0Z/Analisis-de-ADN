904e1c90a89e84fdcecbe928604a7f049c1c22c5
kevin <kev.ubedo@gmail.com>
2019-11-14 01:37:00 -0600

    Compartir con Facebook

--- src/components/ResultsTable.vue
+++ src/components/ResultsTable.vue
@@ -28,7 +28,18 @@
         <v-data-iterator hide-default-footer no-data-text="">
             <template  v-slot:header>
                 <v-toolbar class="mt-2" color="#20004d" dark flat>
-                    <v-btn color = "#3b1070" style='box-shadow: 5px 3px #20004d'>Compartir en Facebook</v-btn>
+                    
+                    <social-sharing url="https://media1.giphy.com/media/YlmI36YAWe7KScC7hK/giphy.gif"
+                    description="Análisis de ADN - ¿Somos familia?"
+                    quote="Su porcentaje de parentesco es "
+                    inline-template>
+                    
+                    <div>
+                        <network network="facebook">
+                            <v-btn color = "#3b1070" style='box-shadow: 5px 3px #20004d'>Compartir en Facebook</v-btn>
+                        </network>
+                    </div>
+                    </social-sharing>
                     <v-spacer></v-spacer>
                     <v-toolbar-title>Análisis de ADN - ¿Somos familia?</v-toolbar-title>
                     <v-spacer></v-spacer>
@@ -38,3 +49,16 @@
         </v-data-iterator>
   </v-container>
 </template>
+
+<script>
+  import SocialSharing from 'vue-social-sharing';
+  export default {
+    components: {
+      SocialSharing
+    },
+    data () {
+      return {
+      }
+    },
+  }
+</script>
